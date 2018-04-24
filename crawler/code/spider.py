a071dbadc1626f08ffad40c20d1b5bcd0d1bfa87
Kunal Desai <kunaljaydesai@users.noreply.github.com>
2018-04-24 02:20:50 -0700

    Update spider.py

--- crawler/code/spider.py
+++ crawler/code/spider.py
@@ -7,61 +7,58 @@ from scrapy import squeue
  
 from article import Article
 try:
-    from urllib.parse import urlparse
+	from urllib.parse import urlparse
 except ImportError:
-    from urlparse import urlparse
- 
+	from urlparse import urlparse
+
 class FakeNewsSpider(scrapy.Spider):
  
-    name = "FNAB"
-    CONCURRENT_REQUESTS = 100
-    REACTOR_THREADPOOL_MAXSIZE = 20
-    tablename = "VISITED_URLS"
-    col1 = "URLS"
-    field_type = 'TEXT'
-    db = os.environ["VISITED_URLS"]
-    conn = sqlite3.connect(db)
-    c = conn.cursor()
-    c.execute('CREATE TABLE IF NOT EXISTS {tn} ({nf} {ft})'\
-              .format(tn = tablename, nf = col1, ft = field_type))
-    SCHEDULER_DISK_QUEUE = 'scrapy.squeues.PickleFifoDiskQueue'
-    SCHEDULER_MEMORY_QUEUE = 'scrapy.squeues.FifoMemoryQueue'
-    custom_settings = {
-       "SCHEDULER_DISK_QUEUE" : SCHEDULER_DISK_QUEUE,
-       "SCHEDULER_MEMORY_QUEUE" : SCHEDULER_MEMORY_QUEUE,
-       "DEPTH_LIMIT" : 15,
-       "DEPTH_PRIORITY": 99
-     }
-    def do_all(self):
-        urls = tuple(open(os.environ["URL_LIST"], 'r'))
-        for url in urls:
-            url = url.rstrip()
-            self.crawler.engine.slot.add_request(scrapy.Request(url=url, callback=self.parse))
-    def start_requests(self):
-	# urls = FakeNewsSpider.og_white_list
-        self.do_all()
+	name = "FNAB"
+	CONCURRENT_REQUESTS = 100
+	REACTOR_THREADPOOL_MAXSIZE = 20
+	tablename = "VISITED_URLS"
+	col1 = "URLS"
+	field_type = 'TEXT'
+	db = os.environ["VISITED_URLS"]
+	conn = sqlite3.connect(db)
+	c = conn.cursor()
+	c.execute('CREATE TABLE IF NOT EXISTS {tn} ({nf} {ft})'\
+			  .format(tn = tablename, nf = col1, ft = field_type))
+	SCHEDULER_DISK_QUEUE = 'scrapy.squeues.PickleFifoDiskQueue'
+	SCHEDULER_MEMORY_QUEUE = 'scrapy.squeues.FifoMemoryQueue'
+	custom_settings = {
+	   "SCHEDULER_DISK_QUEUE" : SCHEDULER_DISK_QUEUE,
+	   "SCHEDULER_MEMORY_QUEUE" : SCHEDULER_MEMORY_QUEUE,
+	   "DEPTH_LIMIT" : 15,
+	   "DEPTH_PRIORITY": 99
+	 }
+	def do_all(self):
 	urls = tuple(open(os.environ["URL_LIST"], 'r'))
-        for url in urls:
-            url = url.rstrip()
-            yield scrapy.Request(url=url, callback=self.parse)
-       
-    # call back for each web page
-    def parse(self, response):
-        # data model for parsing
-        data = Article(response)
-        data.save_to_file()
-        # iterate through the links on the page and continue crawling
-        gen = (link for link in data.get_links())
-        print(len(self.crawler.engine.slot.inprogress))
-        for link in gen:
-     
-            # check to see if url exists in sqllite db
-            row = FakeNewsSpider.c.execute("SELECT * FROM " + FakeNewsSpider.tablename + " WHERE " + FakeNewsSpider.col1 + " = ?", (link,))
-            # if it does then do not yield, if it doesn't then add it to db and yield
-            if row.fetchone() == None:
-                FakeNewsSpider.c.execute("INSERT INTO " + FakeNewsSpider.tablename + " (" + FakeNewsSpider.col1 + ") VALUES (?)", (link,))
-                FakeNewsSpider.conn.commit()
-                req = response.follow(link, callback=self.parse)
-                yield req
- 
-        FakeNewsSpider.conn.commit()
+	for url in urls:
+			url = url.rstrip()
+			self.crawler.engine.slot.add_request(scrapy.Request(url=url, callback=self.parse))
+	def start_requests(self):
+	# urls = FakeNewsSpider.og_white_list
+		self.do_all()
+		urls = tuple(open(os.environ["URL_LIST"], 'r'))
+		for url in urls:
+			url = url.rstrip()
+			yield scrapy.Request(url=url, callback=self.parse)
+	# call back for each web page
+	def parse(self, response):
+		# data model for parsing
+		data = Article(response)
+		data.save_to_file()
+		# iterate through the links on the page and continue crawling
+		gen = (link for link in data.get_links())
+		print(len(self.crawler.engine.slot.inprogress))
+		for link in gen:
+			# check to see if url exists in sqllite db
+			row = FakeNewsSpider.c.execute("SELECT * FROM " + FakeNewsSpider.tablename + " WHERE " + FakeNewsSpider.col1 + " = ?", (link,))
+			# if it does then do not yield, if it doesn't then add it to db and yield
+			if row.fetchone() == None:
+				FakeNewsSpider.c.execute("INSERT INTO " + FakeNewsSpider.tablename + " (" + FakeNewsSpider.col1 + ") VALUES (?)", (link,))
+				FakeNewsSpider.conn.commit()
+				req = response.follow(link, callback=self.parse)
+				yield req
+		FakeNewsSpider.conn.commit()
